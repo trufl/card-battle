92f0d983540fe68fc17203d6ef965aff8b2e4def
nixon <jeromenixon95@gmail.com>
2022-10-27 13:51:40 -0700

    added card seeds boiler plate

---
+++ seeds/cardseeds.js
@@ -0,0 +1,86 @@
+const { DefaultDeserializer } = require('v8');
+const { Cards } = require('../models');
+
+const cardData = [
+    {
+    card_name: 'card1',
+    attackStat: '',
+    DefenseStat: '',
+    // filename?
+    card_image: 'card1-cardimg.jpg'
+    // image relative to public / assets/images
+},
+{
+    card_name: 'card2',
+    attackStat: '',
+    DefenseStat: '',
+    // filename?
+    card_image: 'card2-cardimg.jpg'
+    // image relative to public / assets/images
+},
+{
+    card_name: 'card3',
+    attackStat: '',
+    DefenseStat: '',
+    // filename?
+    card_image: 'card3-cardimg.jpg'
+    // image relative to public / assets/images
+},
+{
+    card_name: 'card4',
+    attackStat: '',
+    DefenseStat: '',
+    // filename?
+    card_image: 'card4-cardimg.jpg'
+    // image relative to public / assets/images
+},
+{
+    card_name: 'card5',
+    attackStat: '',
+    DefenseStat: '',
+    // filename?
+    card_image: 'card5-cardimg.jpg'
+    // image relative to public / assets/images
+},
+{
+    card_name: 'card6',
+    attackStat: '',
+    DefenseStat: '',
+    // filename?
+    card_image: 'card6-cardimg.jpg'
+    // image relative to public / assets/images
+},
+{
+    card_name: 'card7',
+    attackStat: '',
+    DefenseStat: '',
+    // filename?
+    card_image: 'card7-cardimg.jpg'
+    // image relative to public / assets/images
+},
+{
+    card_name: 'card8',
+    attackStat: '',
+    DefenseStat: '',
+    // filename?
+    card_image: 'card8-cardimg.jpg'
+    // image relative to public / assets/images
+},
+{
+    card_name: 'card9',
+    attackStat: '',
+    DefenseStat: '',
+    // filename?
+    card_image: 'card9-cardimg.jpg'
+    // image relative to public / assets/images
+},
+{
+    card_name: 'card10',
+    attackStat: '',
+    DefenseStat: '',
+    // filename?
+    card_image: 'card10-cardimg.jpg'
+    // image relative to public / assets/images
+},
+
+]
