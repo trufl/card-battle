32dcb189c052499344a3b7ee5865eb80d2b43d96
T <endlesstruffle80@gmail.com>
2022-10-27 10:48:31 -0700

    save work

---
+++ public/js/card-picker.js
@@ -0,0 +1,44 @@
+const cards = [
+    {
+        id: 1,
+        name: 'gandalf',
+        attack: 100,
+        defense: 100,
+        getStats: () => `Attack: ${this.attack} Defense: ${this.defense}`,
+        getName: () => this.name,
+    },
+    {
+        id: 2,
+        name: 'sam',
+        attack: 200,
+        defense: 200,
+        getStats: () => `Attack: ${this.attack} Defense: ${this.defense}`,
+        getName: () => this.name,
+    },
+    {
+        id: 3,
+        name: 'frodo',
+        attack: 300,
+        defense: 300,
+        getStats: () => `Attack: ${this.attack} Defense: ${this.defense}`,
+        getName: () => this.name,
+    },
+    {
+        id: 4,
+        name: 'eragon',
+        attack: 400,
+        defense: 400,
+        getStats: () => `Attack: ${this.attack} Defense: ${this.defense}`,
+        getName: () => this.name,
+    },
+];
+
+
+
+const pickUsersCard = async () => {
+    //const cards = await getCards();
+
+
+
+
+} 
