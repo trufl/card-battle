8f22eff7f4944d6921d25c52d2e2c6133d7287b2
T <endlesstruffle80@gmail.com>
2022-10-28 10:37:26 -0700

    Adds ai choice function and attempts to fix bug in turn based function

--- public/js/turn.js
+++ public/js/turn.js
@@ -1,16 +1,51 @@
 function turnBased () {
-    const turn = gameStats.getTurn();
+    const playerAlive = gameStats.player.isAlive();
+    const aiAlive = gameStats.player.isAlive();
 
-    if(turn) {
-        gameStats.setTurn(false);
-        getPlayerChoice();
+    if(playerAlive && aiAlive) {
+        const turn = gameStats.getTurn();
+
+        if(turn) {
+            gameStats.setTurn(false);
+            getPlayerChoice();
+        } else {
+            gameStats.setTurn(true);
+            getAiChoice();
+        }
     } else {
-        gameStats.setTurn(true);
-        getAiChoice();
+        endGame();
     }
 }
 
 
 function getAiChoice() {
-    //const ready = document.getAttribute('data-ready');
+    const isAlive = gameStats.ai.isAlive();
+
+    if(isAlive) {
+        const health = gameStats.ai.checkHealth();
+
+        if(health > 100) {
+            attackCb();
+        } else {
+            const play = Math.floor(Math.random * 3) + 1
+
+            const lastAttack = gameStats.ai.checkLastMove();
+
+            if(!lastAttack) {
+                attackCb();
+            } else {
+                
+                if(play === 1){
+                    attackCb();
+                } else if (play === 2){
+                    defendeCb();
+                } else {
+                    console.log(`Error, play = ${play}`);
+                }
+            }
+        }
+    } else {
+        endGame();
+    }
+
 }
