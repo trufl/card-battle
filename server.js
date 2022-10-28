20e54d1526ac3a4050d26efa9c1ad7e5ae702196
97ToyoTaco <leocubshat@gmail.com>
2022-10-28 14:09:10 -0700

    added tailwind, full screen map, and some handlebars layout

--- server.js
+++ server.js
@@ -13,8 +13,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// const hbs = exphbs.create({ helpers });
-
+const hbs = exphbs.create({  });
 
 const sess = {
     secret: process.env.SECRET,
@@ -28,7 +27,7 @@ const sess = {
 
 // app.use(session(sess));
 
-// app.engine('handlebars', hbs.engine);
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
