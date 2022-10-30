0fc7cf889ff3b487437804587bd404a9148d872b
97ToyoTaco <leocubshat@gmail.com>
2022-10-29 20:16:56 -0700

    added formatting and styling to signup, login, home, and main.handlebars

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./views/**/*.handlebars"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'press-start': ['"Press Start 2P"', 'cursive'],
+      }
+    },
   },
   plugins: [],
 }
