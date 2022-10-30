9a50cfbacb5b9f472b0056287811d8f91bbfa6dc
Morgan Tolman <morgan.tolman04@gmail.com>
2022-10-30 14:27:19 -0700

    Got signup to work

--- controllers/apiRoutes/apiRoutes.js
+++ controllers/apiRoutes/apiRoutes.js
@@ -47,4 +47,23 @@ router.post('/logout', (req, res) => {
     }
 });
 
+router.post('/signup', async (req, res) => {
+    try{
+        const userData = await User.create({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+        });
+        
+        req.session.save(() => {
+            req.session.loggedIn = true;
+            req.session.user_id = userData.id;
+            res.status(200).json(userData);
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
